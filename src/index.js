da5f0bbb5c0ad68344a5c6ec93e0daef4689f473
ivasiget <iva.siget@gmail.com>
2022-09-26 19:56:57 +0200

    added date&time

--- src/index.js
+++ src/index.js
@@ -3,6 +3,69 @@ let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
+let now = new Date();
+console.log(now);
+
+let hour = now.getHours();
+let minutes = now.getMinutes();
+let timeElement = document.querySelector("#time");
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+timeElement.innerHTML = `${hour}:${minutes}`;
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let dayElement = document.querySelector("#day");
+dayElement.innerHTML = day;
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "May",
+  "April",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let monthElement = document.querySelector("#month");
+monthElement.innerHTML = month;
+
+let sequence = ["st", "nd", "rd", "th"];
+
+if (now.getDate() === 1) {
+  sequence = sequence[0];
+}
+if (now.getDate() === 2) {
+  sequence = sequence[1];
+}
+if (now.getDate() === 3) {
+  sequence = sequence[2];
+} else {
+  sequence = sequence[3];
+}
+
+let date = now.getDate();
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = `${date}${sequence}`;
+
 function showTemperature(response) {
   console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
